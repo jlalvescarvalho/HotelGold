14ab2653fb20a174ba6b948fb56de74b259985cd
EvertonTenorio <everton.tenorio10@gmail.com>
2018-01-23 13:23:05 -0300

    Problemas da Tela Resolvido

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -9,12 +9,11 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("TelaInicial.fxml"));
-        Scene scene = new Scene(root);
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("Tela Inicial");
-        primaryStage.show();
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("TelaInicial.fxml"));      
+        Scene scene = new Scene(root);   
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[] args) {
--- src/gui/TelaInicialController.java
+++ src/gui/TelaInicialController.java
@@ -1,16 +1,26 @@
 package gui;
 
-import java.awt.event.ActionEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 
-public class TelaInicialController implements Initializable{
+public class TelaInicialController implements Initializable {
     
+    @FXML
+    private Label label;
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+        label.setText("Hello World!");
+    }
     
     @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        
-    }
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
 }
