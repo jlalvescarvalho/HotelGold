6cda40c9160c5e6adf0ab5386586137294c73b23
Luciano <lucianoalves413@gmail.com>
2018-01-27 20:55:20 -0300

    Funcionando

--- src/gui/controllers/TelaHospedeController.java
+++ src/gui/controllers/TelaHospedeController.java
@@ -2,12 +2,27 @@ package gui.controllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
 
 public class TelaHospedeController implements Initializable {
     
+    private TextField txtNome;
+    private TextField txtCpf;
+    private TextField txtTelefone;
+    
+    protected void cadastrarHospede(ActionEvent e){
+        
+        String nome, cpf, telefone;
+        nome = txtNome.getText();
+        nome = txtCpf.getText();
+        nome = txtTelefone.getText();  
+    }
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
+              
     }     
 }
