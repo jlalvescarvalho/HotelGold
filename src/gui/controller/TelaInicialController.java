4329a4ceae65b32308b5650d3b0f4b598d4b40ca
Luciano <lucianoalves413@gmail.com>
2018-01-27 22:26:03 -0300

    Começo das ações dos botoes

--- src/gui/controller/TelaInicialController.java
+++ src/gui/controller/TelaInicialController.java
@@ -2,12 +2,28 @@ package gui.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javax.swing.JOptionPane;
+
 
 public class TelaInicialController implements Initializable {
     
+    @FXML
+    private Button btHospede;
+    
+    @FXML
+    public void abrirTelaHospede(MouseEvent event){
+        JOptionPane.showMessageDialog(null, "fufou");
+    }
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        btHospede.setOnMouseClicked((event) -> {
+            abrirTelaHospede(event);
+        });
     }     
 }
