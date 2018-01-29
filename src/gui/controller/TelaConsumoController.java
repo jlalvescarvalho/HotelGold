1565b306518c4abeaff56477e8d7913c05c60eb5
JenilsinhoMoraes <jenilsinhomores@gmail.com>
2018-01-29 16:20:57 -0200

    Adicionar consumo ok

--- src/gui/controller/TelaConsumoController.java
+++ src/gui/controller/TelaConsumoController.java
@@ -2,7 +2,10 @@ package gui.controller;
 
 import fachada.Hotel;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
@@ -13,6 +16,7 @@ import javax.swing.JOptionPane;
 import negocio.entidade.BebidasEnum;
 import negocio.entidade.RefeicaoEnum;
 import negocio.entidade.Reserva;
+import negocio.entidade.TipoReservaEnum;
 import negocio.execao.reserva.ReservaNaoExisteException;
 
 public class TelaConsumoController implements Initializable {
@@ -62,6 +66,66 @@ public class TelaConsumoController implements Initializable {
             JOptionPane.showMessageDialog(null, e.getMessage());
         }
 
+    }
+    
+    @FXML
+    protected void adicionarBebida(){
+            try {
+            reserva = Hotel.getInstance().buscarReserva(Long.parseLong(idAdicionar.getText()));
+            reserva.getHospede().getCartaoConsumo().consumir(listaBebidas.getValue());
+            JOptionPane.showMessageDialog(null, "Bebida adicionada");
+
+        } catch (ReservaNaoExisteException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+
+    }
+    
+    @FXML
+    protected void adicionarRefeicao(){
+            try {
+            reserva = Hotel.getInstance().buscarReserva(Long.parseLong(idAdicionar.getText()));
+            reserva.getHospede().getCartaoConsumo().consumir(listaRefeicoes.getValue());
+            JOptionPane.showMessageDialog(null, "Refeição adicionada");
+
+        } catch (ReservaNaoExisteException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+
+    }
+    
+    protected void preencherTipoBebidas() {
+        ArrayList<BebidasEnum> tipoBebida = new ArrayList<>();
+        tipoBebida.add(BebidasEnum.Agua);
+        tipoBebida.add(BebidasEnum.CocaCola);
+        tipoBebida.add(BebidasEnum.Fanta);
+        tipoBebida.add(BebidasEnum.Suco);
+        tipoBebida.add(BebidasEnum.Whisky);
+
+        ObservableList<BebidasEnum> itens = FXCollections.observableArrayList(tipoBebida);
+        listaBebidas.setItems(itens);
+        
+    
+    
+    }   
+    
+    @FXML
+    protected void preencherTipoRefeicao(){
+        preencherTipoBebidas();
+        
+        ArrayList<RefeicaoEnum> tipoRefeicao = new ArrayList<>();
+        tipoRefeicao.add(RefeicaoEnum.Almoco);
+        tipoRefeicao.add(RefeicaoEnum.CafeDaManha);
+        tipoRefeicao.add(RefeicaoEnum.Janta);
+        
+        ObservableList<RefeicaoEnum> itens = FXCollections.observableArrayList(tipoRefeicao);
+        listaRefeicoes.setItems(itens);
+        
+    }
+    
+    @FXML
+    protected void listarConsumo(){
+        
     }
 
     @Override
