c05bb900882dc91d72eaa6ba0344ee2d1f6e35d8
JenilsinhoMoraes <jenilsinhomores@gmail.com>
2018-01-28 23:19:27 -0200

    Atributos tela consumo adicionados

--- src/gui/controller/TelaConsumoController.java
+++ src/gui/controller/TelaConsumoController.java
@@ -1,24 +1,39 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package gui.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import negocio.entidade.BebidasEnum;
+import negocio.entidade.RefeicaoEnum;
 
-/**
- * FXML Controller class
- *
- * @author jenil
- */
 public class TelaConsumoController implements Initializable {
+    
+    @FXML
+    private TextField idAdicionar;
+    @FXML
+    private TextField idRemover;
+    @FXML
+    private Label nomeAdicionar;
+    @FXML
+    private Label cpfAdicionar;
+    @FXML
+    private Label nomeRemover;
+    @FXML
+    private Label cpfRemover;
+    @FXML
+    private ComboBox<BebidasEnum> listaBebidas = new ComboBox<>();
+    @FXML
+    private ComboBox<RefeicaoEnum> listaRefeicoes = new ComboBox<>();
+    @FXML
+    private ComboBox<Enum> listaRemocao = new ComboBox<>();
+    @FXML
+    private ListView<Enum> listaConsumo = new ListView<>();
 
-    /**
-     * Initializes the controller class.
-     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
